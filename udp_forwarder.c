b9409327d57cec060bc958162027cfd73cfec450
Autrin <autrinhakimi@gmail.com>
2024-09-30 17:13:17 -0500

    binding and error checking

--- udp_forwarder.c
+++ udp_forwarder.c
@@ -46,6 +46,13 @@ int main(int argc, char **argv)
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    
+
+    if(bind(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        perror("Error binding socket");
+        close(sock_fd);
+        exit(1);
+    }
+    printf("Socket bound\n");
+
     return 0;
 }
