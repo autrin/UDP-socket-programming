44fd4458a484b378e98b0ad964da0ecd239c9f05
Autrin <autrinhakimi@gmail.com>
2024-10-10 15:18:09 -0500

    added the source and destination addresses

--- udp_forwarder.c
+++ udp_forwarder.c
@@ -31,18 +31,20 @@ int main(int argc, char *argv[])
         printf("Usage: %s <SOURCE_IP> <SOURCE_PORT> <DESTINATION_IP> <DESTINATION_PORT> <LOSS_RATE>\n", argv[0]);
         exit(1);
     }
-    // the ports should be between 1024 and 65535
-    int source_ip = inet_addr(argv[1]);
+    char *source_ip = argv[1];
     int source_port = atoi(argv[2]);
-    int destination_ip = inet_addr(argv[3]);
-    int destination_port = atoi(argv[4]);
-    int loss_rate = atoi(argv[5]); // number of the packets out of 1000 that are intentionally dropped (not forwarded)
-    
-    if(source_port  < 1024 || destination_port < 1024 || source_port > 65535 || destination_port > 65535){
-        perror("Invalid port number. Port number should be between 1024 and 65535");
+    char *dest_ip = argv[3];
+    int dest_port = atoi(argv[4]);
+    int loss_rate = atoi(argv[5]);
+
+    if (source_port < 1024 || source_port > 65535 || dest_port < 1024 || dest_port > 65535) {
+        perror("Invalid port number. Port numbers should be between 1024 and 65535");
         exit(1);
     }
-
+    
+    int sock_fd;
+    struct sockaddr_in src_addr, dest_addr;
+    
     
     return 0;
 }
